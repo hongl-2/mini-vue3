07c49f06fd30a1463dba24bd4cb3023440218f50
hongl-1 <hongl_gitee@163.com>
2022-07-07 23:25:23 +0800

    feat: add readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from './effect';
+import { warn } from '../shared';
 
 export function reactive(raw) {
   return new Proxy(raw, {
@@ -13,6 +14,21 @@ export function reactive(raw) {
       // 触发依赖更新
       trigger(target, key)
       return result
+    }
+  })
+}
+
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get (target, key) {
+      const res = Reflect.get(target, key)
+      // 依赖收集
+      track(target, key)
+      return res
+    },
+    set (target, key, value) {
+      warn(`${key as string} is readonly, cannot been set, target:${target}`)
+      return true
     }
   })
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,5 @@
 export const extend = Object.assign
+
+export function warn(text: string) {
+  console.warn(`[mini vue3 warn] ${text}`)
+}
